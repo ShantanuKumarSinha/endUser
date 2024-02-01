aead507a08134f61b81ab982620d07367d0ceaca
Shantanu Kumar <shantanukumar@Shantanus-MacBook-Air.local>
2024-02-01 22:09:03 +0530

    Initial commit

---
+++ src/main/java/com/endUser/endUser/config/KafkaConfig.java
@@ -0,0 +1,19 @@
+package com.endUser.endUser.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@Configuration
+public class KafkaConfig {
+
+    private Logger log = LoggerFactory.getLogger(KafkaConfig.class);
+
+    @KafkaListener(topics = AppConstants.LOCATION_UPDATE_TOPIC, groupId = AppConstants.GROUP_ID)
+    public void updatedLocation(String value){
+        log.info(value);
+        log.info("___");
+    }
+}
